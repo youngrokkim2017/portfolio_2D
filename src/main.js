92b704f6467d61028d196d8794b236cac3ae84c7
Patrick Kim <patrick.yrkim@gmail.com>
2024-04-18 23:20:01 -0700

    Update animations

--- src/main.js
+++ src/main.js
@@ -5,5 +5,10 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceY: 31,
     anims: {
         "idle-down": 936,
+        "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+        "idle-side": 975,
+        "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+        "idle-up": 1014,
+        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
     }
 })
