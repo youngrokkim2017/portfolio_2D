fa3c3217ba7949e663de051af5656690c81ed3c4
Patrick Kim <patrick.yrkim@gmail.com>
2024-04-20 21:47:10 -0700

    Add player

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { k } from "./kaboomCtx";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
-    sliceX: 39,
-    sliceY: 31,
-    anims: {
-        "idle-down": 936,
-        "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
-        "idle-side": 975,
-        "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
-        "idle-up": 1014,
-        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
-    }
+  sliceX: 39,
+  sliceY: 31,
+  anims: {
+    "idle-down": 936,
+    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+    "idle-side": 975,
+    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+    "idle-up": 1014,
+    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+  }
 })
 
 k.loadSprite("map", "./map.png")
@@ -21,7 +21,25 @@ k.scene("main", async () => {
   const mapData = await (await fetch("./map.json")).json();
   const layers = mapData.layers;
 
-  const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+  const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+//   const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", { anim: "idle-down" }),
+    k.area({
+      shape: new k.Rect(k.vec2(0, 3), 10, 10),
+    }),
+    k.body(),
+    k.anchor("center"),
+    k.pos(),
+    k.scale(scaleFactor),
+    {
+      speed: 250,
+      direction: "down",
+      isInDialogue: false,
+    },
+    "player",
+  ]);
 });
 
 k.go("main");
